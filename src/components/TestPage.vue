797057709b3563bda266853206255179408dd754
HLN177 <harryleen177@gmail.com>
2022-09-27 20:50:29 +0100

    fix: update template

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="about">
-    {{count}}
+  <div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
 export default {
   components: {
   },
@@ -17,9 +15,6 @@ export default {
     };
   },
   computed: {
-    ...mapState({
-      count: state => state.count
-    })
   },
   watch: {
     
@@ -34,15 +29,4 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-// .fuck {
-//   width: 100%;
-//   height: 100%;
-// }
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
